56ac8bdb4d0257d8aa6bb7d734328a8defccf5ba
Kanyadet <oduorgeofrey141@gmail.com>
2024-05-04 03:39:34 +0300

    onooo

--- GoogleAuthlogin.js
+++ GoogleAuthlogin.js
@@ -3,10 +3,10 @@ var uiConfig = {
   signInSuccessUrl: 'https://kanyadetschool-1.web.app/loginwindow2.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-          // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           //firebase.auth.GithubAuthProvider.PROVIDER_ID,
   ],
